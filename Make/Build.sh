5288299f75ea57b5a6df7eebad921f81006b52bf
yannis <yannis@tucuxi.ch>
2017-06-19 15:07:30 +0200

    TUC-45 Fixed Build.sh

--- Make/Build.sh
+++ Make/Build.sh
@@ -3,15 +3,14 @@
 TUCUXI_ROOT=/srv/TucuData/jenkins/Jobs/Tucuxi/workspace
 export TUCUXI_ROOT=/srv/TucuData/jenkins/Jobs/Tucuxi/workspace
 
-if [ "$1" == "clean" ]; then
-makecmd=clean
+if test "$1" = "clean"; then
+MAKECMD=clean
 fi
 
 for MODULE in TucuCommon TucuCore TucuCli
 do
    cd $TUCUXI_ROOT/Src/$MODULE
-#   make TARGET=LINUX 
-   ls -la 
+   make TARGET=LINUX $MAKECMD   
 done
 
 
