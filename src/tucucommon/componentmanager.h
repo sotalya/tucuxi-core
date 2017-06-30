4a5ddcb2e7e893237cb808ba47ddb860ccfe79ab
yannis <yannis@tucuxi.ch>
2017-06-30 13:38:07 +0200

    TUC-45 Added an UML diagram for event series in the processing layer.

--- src/tucucommon/componentmanager.h
+++ src/tucucommon/componentmanager.h
@@ -39,68 +39,68 @@ typedef Interface* (*TComponentFactory)();
 class ComponentManager
 {
 public:
-	/// \brief The only way to get a hold on the component manager
-	/// @return The component manager itself
-	static ComponentManager* getInstance();
+    /// \brief The only way to get a hold on the component manager
+    /// @return The component manager itself
+    static ComponentManager* getInstance();
 
 public:
-	/// \brief Register a factory and associate it with a name.
-	/// @param _name The name that will allow creating instances via method createComponent
-	/// @param _pFactory A pointer to a function that "knows" how to create a component of the given type.
-	/// @return None
-	void registerComponentFactory(const std::string &_name, TComponentFactory _pFactory);
+    /// \brief Register a factory and associate it with a name.
+    /// @param _name The name that will allow creating instances via method createComponent
+    /// @param _pFactory A pointer to a function that "knows" how to create a component of the given type.
+    /// @return None
+    void registerComponentFactory(const std::string &_name, TComponentFactory _pFactory);
 
-	/// \brief Register a component so to make it available globally to the rest of the application.
-	/// @param _name The name that will allow other parts of the software to retrieve the component.
-	/// @param _pComponent A pointer to the interface of an existing component.
-	/// @return None
-	void registerComponent(const std::string &_name, Interface *_pComponent);
+    /// \brief Register a component so to make it available globally to the rest of the application.
+    /// @param _name The name that will allow other parts of the software to retrieve the component.
+    /// @param _pComponent A pointer to the interface of an existing component.
+    /// @return None
+    void registerComponent(const std::string &_name, Interface *_pComponent);
 
-	/// \brief Unregister a previously registered component.
-	/// @param _name The name of the component
-	/// @return None
-	void unregisterComponent(const std::string &_name);
+    /// \brief Unregister a previously registered component.
+    /// @param _name The name of the component
+    /// @return None
+    void unregisterComponent(const std::string &_name);
 
-	/// \brief Create an instance of a component from a previously registered factory.
-	/// @param _factoryName The name of the factory.
-	/// @param T The type of the desired interface
-	/// @return A pointer on the specified interface of the newly created component
-	template <typename T> T* createComponent(const std::string &_factoryName)
-	{
-		if (m_factories.end() != m_factories.find(_factoryName))
-		{
-			Interface *cmp = m_factories[_factoryName]();
-			if (cmp != nullptr) {
-				return cmp->getInterface<T>();
-			}
-		}
-		return nullptr;
-	}
+    /// \brief Create an instance of a component from a previously registered factory.
+    /// @param _factoryName The name of the factory.
+    /// @param T The type of the desired interface
+    /// @return A pointer on the specified interface of the newly created component
+    template <typename T> T* createComponent(const std::string &_factoryName)
+    {
+        if (m_factories.end() != m_factories.find(_factoryName))
+        {
+            Interface *cmp = m_factories[_factoryName]();
+            if (cmp != nullptr) {
+                return cmp->getInterface<T>();
+            }
+        }
+        return nullptr;
+    }
 
-	/// \brief Get a previously registered component.
-	/// @param _name The name of the component.
-	/// @param T The type of the desired interface
-	/// @return A pointer on the specified interface of the specified component
-	template <typename T> T* getComponent(const std::string &_name)
-	{
-		std::string interfaceName = typeid(T*).name();
-		if (m_components.end() != m_components.find(_name))
-		{
-			Interface* itf = m_components[_name];
-			if (itf != nullptr) {
-				return itf->getInterface<T>();
-			}
-		}
-		return nullptr;
-	}
-
-private:
-	/// \brief Constructor. Used internal to create the singleton instance.
-	ComponentManager();
+    /// \brief Get a previously registered component.
+    /// @param _name The name of the component.
+    /// @param T The type of the desired interface
+    /// @return A pointer on the specified interface of the specified component
+    template <typename T> T* getComponent(const std::string &_name)
+    {
+        std::string interfaceName = typeid(T*).name();
+        if (m_components.end() != m_components.find(_name))
+        {
+            Interface* itf = m_components[_name];
+            if (itf != nullptr) {
+                return itf->getInterface<T>();
+            }
+        }
+        return nullptr;
+    }
 
 private:
-	std::map<std::string, TComponentFactory> m_factories;	/// The list of registered factories
-	std::map<std::string, Interface*> m_components;			/// The lisf of registered components
+    /// \brief Constructor. Used internal to create the singleton instance.
+    ComponentManager();
+
+private:
+    std::map<std::string, TComponentFactory> m_factories;   /// The list of registered factories
+    std::map<std::string, Interface*> m_components;         /// The lisf of registered components
 };
 
 }
