3b9100c517c3e2155c7a39f7d964ea85015b0f81
magali <magali.frohlich@heig-vd.ch>
2017-07-04 10:40:10 +0200

    TUC-67 Add spdlog logger in logger component

--- src/tucucommon/logger.cpp
+++ src/tucucommon/logger.cpp
@@ -4,60 +4,36 @@
 
 #include "tucucommon/logger.h"
 
-virtual void Logger::debug(const char* msg)
+Logger::~Logger()
 {
-
+    // Under VisualStudio, this must be called before main finishes to workaround a known VS issue
+    spdlog::drop_all();
 }
 
-//template <typename Arg1, typename... Args>
-//virtual void Logger::debug(const char* fmt, const Arg1 &arg1, const Args&... args)
-//{
-
-//}
+virtual void Logger::debug(const char* msg)
+{
+    m_logger->debug(msg);
+}
 
 virtual void Logger::info(const char* msg)
 {
-
+    m_logger->info(msg);
 }
-
-//template <typename Arg1, typename... Args>
-//virtual void Logger::info(const char* fmt, const Arg1 &arg1, const Args&... args)
-//{
-
-//}
 
 virtual void Logger::warn(const char* msg)
 {
-
+    m_logger->warn(msg);
 }
-
-//template <typename Arg1, typename... Args>
-//virtual void Logger::warn(const char* fmt, const Arg1 &arg1, const Args&... args)
-//{
-
-//}
 
 virtual void Logger::error(const char* msg)
 {
-
+    m_logger->error(msg);
 }
-
-//template <typename Arg1, typename... Args>
-//virtual void Logger::error(const char* fmt, const Arg1 &arg1, const Args&... args)
-//{
-
-//}
 
 virtual void Logger::critical(const char* msg)
 {
-
+    m_logger->critical(msg);
 }
-
-//template <typename Arg1, typename... Args>
-//virtual void Logger::critical(const char* fmt, const Arg1 &arg1, const Args&... args)
-//{
-
-//}
 
 virtual Tucuxi::Common::Interface* Logger::getInterface(const std::string &_name)
 {
@@ -67,4 +43,55 @@ virtual Tucuxi::Common::Interface* Logger::getInterface(const std::string &_name
 Logger::Logger()
 {
     registerInterface(dynamic_cast<ILogger*>(this));
+
+    // Spdlog will not throw exceptions while logging but it might throw during
+    // the construction logger.
+    try
+    {
+        // Creating loggers with multiple sinks. Every log are written in
+        // multiple target (e.g file, console).
+
+        // Sinks are the objects that actually write the log to their target.
+        // Each sink should be responsible for only single target
+        // (e.g file, console, db)
+
+        // Thread safe sinks: sinks ending with _mt (e.g daily_file_sink_mt)
+        // Non thread safe sinks: sinks ending with _st (e.g daily_file_sink_st)
+
+        std::vector<spdlog::sink_ptr> sinks;
+
+        // Sink for console
+
+        // Windows
+        sinks.push_back(std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>());
+
+        // Linux
+        // sinks.push_back(std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>());
+
+        // Sink for rotating file
+        sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logfile", 1024 * 1024 * 5, 3));
+
+        // Sink for daily file
+        // sinks.push_back(std::make_shared<spdlog::sinks::daily_file_sink_mt>("logfile", "txt", 23, 59));
+
+        // Create logger
+        m_logger = std::make_shared<spdlog::logger>("tucuxi_logger", begin(sinks), end(sinks));
+
+        // Register it if you need to access it globally. In this case is useless.
+        // spdlog::register_logger(m_logger);
+        // m_logger = spdlog::get("tucuxi_logger");
+
+        // We cas use multiple loggers with the same sink.
+        // E.g multiple pattern can be written in the same file.
+        // auto sharedFileSink = std::make_shared<spdlog::sinks::simple_file_sink_mt>("fileName.txt");
+        // auto firstLogger = std::make_shared<spdlog::logger>("firstLoggerName", sharedFileSink);
+        // auto secondLogger = std::make_unique<spdlog::logger>("secondLoggerName", sharedFileSink);
+
+        m_logger.set_pattern("TUCUXI [%H:%M:%S %z] [thread %t] %v");
+
+    }
+    catch (const spdlog::spdlog_ex& ex)
+    {
+        std::cout << "Log initialization failed: " << ex.what() << std::endl;
+    }
 }
--- src/tucucommon/logger.h
+++ src/tucucommon/logger.h
@@ -7,41 +7,28 @@
 
 #include "tucucommon/component.h"
 #include "tucucommon/ilogger.h"
+#include "spdlog/spdlog.h"
 
 class Logger : public Tucuxi::Common::Component, ILogger
 {
 
 public:
+    ~Logger();
+
     virtual void debug(const char* msg);
-
-//    template <typename Arg1, typename... Args>
-//    virtual void debug(const char* fmt, const Arg1 &arg1, const Args&... args);
-
     virtual void info(const char* msg);
-
-//    template <typename Arg1, typename... Args>
-//    virtual void info(const char* fmt, const Arg1 &arg1, const Args&... args);
-
     virtual void warn(const char* msg);
-
-//    template <typename Arg1, typename... Args>
-//    virtual void warn(const char* fmt, const Arg1 &arg1, const Args&... args);
-
     virtual void error(const char* msg);
-
-//    template <typename Arg1, typename... Args>
-//    virtual void error(const char* fmt, const Arg1 &arg1, const Args&... args);
-
     virtual void critical(const char* msg);
-
-//    template <typename Arg1, typename... Args>
-//    virtual void critical(const char* fmt, const Arg1 &arg1, const Args&... args);
 
 protected:
     virtual Tucuxi::Common::Interface* getInterface(const std::string &_name);
 
 private:
     Logger();
+
+private:
+    std::shared_ptr<spdlog::logger> m_logger;
 };
 
 #endif // TUCUXI_TUCUCOMMON_LOGGER_H
