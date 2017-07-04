fd1f8a3458f88dd2fa865eb3f45e5bb662e92040
magali <magali.frohlich@heig-vd.ch>
2017-07-04 16:09:32 +0200

    TUC-67 Add unit test for logger component

--- test/tucucommon/test_logger.h
+++ test/tucucommon/test_logger.h
@@ -2,6 +2,12 @@
 #include "fructose/fructose.h"
 #include "tucucommon/logger.h"
 
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <regex>
+
 class some_class {};
 std::ostream& operator<<(std::ostream& os, const some_class& c)
 { 
@@ -10,27 +16,45 @@ std::ostream& operator<<(std::ostream& os, const some_class& c)
 
 struct TestLogger : public fructose::test_base<TestLogger> 
 {
-	void basic(const std::string& _testName)
-	{
-		Logger logger;
-		logger.debug("Tcho les topiots");
-		logger.info("Tcho les {}", "topiots");
-		logger.warn("{} + {} = {}", 2, 2, 4);
-		logger.error("Tcho les topiots");
-		logger.critical("Tcho les topiots");
+    void basic(const std::string& _testName)
+    {
+        std::cout << _testName << std::endl;
 
-		// Todo: Check content of log file...
-	}
+        Tucuxi::Common::Logger logger;
+        logger.debug("Tcho les topiots");
+//        logger.info("Tcho les {}", "topiots");
+//        logger.warn("{} + {} = {}", 2, 2, 4);
+        logger.error("Tcho les topiots");
+        logger.critical("Tcho les topiots");
 
-	void logMyClass(const std::string& _testName)
-	{
-		Logger logger;
- 		some_class c;
- 		logger.info("Logging a custom class with operator<<: {}", c);
+        // Check content of log file :
+        std::ifstream infile("/home/mylag/reds/Tucuxi/tucuxi/bin/MyLogFile.txt");
 
- 		// Todo: Check content of log file...
-	}
+        std::string str;
+
+        int diff = 0;
+
+        std::getline(infile, str);
+        diff = std::regex_match (str, std::regex("\\[[0-9\\-\\:\\.\\ ]*\\] \\[m_logger\\] \\[debug\\] Tcho les topiots"));
+        fructose_assert(diff != 0);
+
+        std::getline(infile, str);
+        diff = std::regex_match (str, std::regex("\\[[0-9\\-\\:\\.\\ ]*\\] \\[m_logger\\] \\[error\\] Tcho les topiots"));
+        fructose_assert(diff != 0);
+
+        std::getline(infile, str);
+        diff = std::regex_match (str, std::regex("\\[[0-9\\-\\:\\.\\ ]*\\] \\[m_logger\\] \\[critical\\] Tcho les topiots"));
+        fructose_assert(diff != 0);
+    }
+
+    void logMyClass(const std::string& _testName)
+    {
+        std::cout << _testName << std::endl;
+
+        Tucuxi::Common::Logger logger;
+        some_class c;
+//        logger.info("Logging a custom class with operator<<: {}", c);
+
+        // Todo: Check content of log file...
+    }
  };
-
- 
-
