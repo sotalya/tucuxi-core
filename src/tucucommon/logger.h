a880c86e7a0118db0d44eaddbaa05a7c95ca2879
magali <magali.frohlich@heig-vd.ch>
2017-07-06 09:57:03 +0200

    TUC-67 Add documentation

--- src/tucucommon/logger.h
+++ src/tucucommon/logger.h
@@ -5,6 +5,11 @@
 #ifndef TUCUXI_TUCUCOMMON_LOGGER_H
 #define TUCUXI_TUCUCOMMON_LOGGER_H
 
+/// \defgroup TucuCommon Tucuxi common library
+/// \brief Common classes and components for Tucuxi applications
+/// This module defines a set of basic and application independent classes and components for
+/// use in other Tucuxi's libraries and applications.
+
 #include "spdlog/spdlog.h"
 
 #include "tucucommon/component.h"
@@ -13,27 +18,64 @@
 namespace Tucuxi {
 namespace Common {
 
+/// \ingroup TucuCommon
+/// \brief Singleton class implement the logging component.
+///
+/// The logging component use spdlog library to create a console logger and a
+/// file rotating logger. Every log messages written throuh the logging methods
+/// are displayed on console and saved in the log file.
+///
+/// The functions under spdlog are thread safe.
+///
+/// The instance of LoggerComponent is managed by LoggerHelper class.
+///
+/// \sa Interface, Component
 class Logger : public Tucuxi::Common::Component, public ILogger
 {
 public:
+    /// \brief Constructor call by LoggerHelper
     ~Logger();
+
+    /// \brief Set minimal level of logging message.
+    /// @param _level Minimal level
+    /// @return None
     void setLevel(spdlog::level::level_enum _level);
 
+    /// \brief Log a "debug" message.
+    /// @param _msg The message to log
+    /// @return None
     virtual void debug(const char* _msg);
+
+    /// \brief Log a "debug" message.
+    /// @param _msg The message to log
+    /// @return None
     virtual void info(const char* _msg);
+
+    /// \brief Log a "debug" message.
+    /// @param _msg The message to log
+    /// @return None
     virtual void warn(const char* _msg);
+
+    /// \brief Log a "debug" message.
+    /// @param _msg The message to log
+    /// @return None
     virtual void error(const char* _msg);
+
+    /// \brief Log a "debug" message.
+    /// @param _msg The message to log
+    /// @return None
     virtual void critical(const char* _msg);
 
 protected:
     virtual Tucuxi::Common::Interface* getInterface(const std::string &_name);
 
 private:
+    /// \brief Constructor call by LoggerHelper
     Logger(const std::string &_filename);
     friend class LoggerHelper;
 
 private:
-    std::shared_ptr<spdlog::logger> m_logger;
+    std::shared_ptr<spdlog::logger> m_logger; /// The pointer to the instance of spdlog logger
 };
 
 }
