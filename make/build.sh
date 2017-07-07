791c1e58a8b1a3f6d4e14478098024b8cb05964c
yannis <yannis@tucuxi.ch>
2017-07-07 15:20:23 +0200

    TUC-45 Added build of external libs in build.sh

--- make/build.sh
+++ make/build.sh
@@ -9,6 +9,17 @@ if test "$1" = "clean"; then
 MAKECMD=clean
 fi
 
+for MODULE in tiny-js-master-20170629
+do
+   cd $TUCUXI_ROOT/libs/$MODULE
+   mkdir -p objs
+   make TARGET=LINUX $MAKECMD $@ 2>&1 | tee objs/build.log
+   if [ ${PIPESTATUS[0]} -eq 2 ]
+   then
+      RESULT=1
+   fi
+done
+
 for MODULE in tucucommon tucucore tucucli
 do
    cd $TUCUXI_ROOT/src/$MODULE
@@ -19,7 +30,6 @@ do
       RESULT=1
    fi
 done
-
 
 for MODULE in tucucommon
 do
