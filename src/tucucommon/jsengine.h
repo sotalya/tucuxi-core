9719b94d1dc824bd7315a1b28f445e8566f35ed4
yannis <yannis@tucuxi.ch>
2017-07-27 11:49:05 +0200

    TUC-80 Added documentation to class JSEngine.

--- src/tucucommon/jsengine.h
+++ src/tucucommon/jsengine.h
@@ -13,21 +13,37 @@ class CTinyJS;
 namespace Tucuxi {
 namespace Common {
 
+/// \ingroup TucuCommon
+/// \brief A simple javascript engine.
 class JSEngine
 {
 public:
+    /// \brief Default constructor
     JSEngine();
     
+    /// \brief Retrieve a variable managed by the engine.
+    /// The variable may have been added via setVariable or during the execution of a script.
+    /// @param _name The name of the variable
+    /// @param _value Its value
+    /// @return True if the variable was found
     bool getVariable(const std::string& _name, std::string& _value);
 
+    /// \brief Create a new variable or change the value of an existing one.
+    /// @param _name The name of the variable
+    /// @param _value An expression to change the value of the variable
+    /// @return True if the operation was successful
     bool setVariable(const std::string& _name, const std::string& _expression);
     
+    /// \brief Execute the specified script
+    /// @param _code The script to execute
+    /// @return True if the execution of the given script was successfull
     bool evaluate(const std::string& _code);
 
+    /// \brief Clear the internal dictionary of variables
     void reset();
 
 private:
-    CTinyJS* m_pEngine;
+    CTinyJS* m_pEngine; /// The "wrapped" Tiny JS script engine
 };
 
 }
