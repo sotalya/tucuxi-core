923a89e17db14ff561b6f5a566b00b9c9f87b72f
Yann Thoma <yann.thoma@heig-vd.ch>
2017-08-18 14:43:03 +0200

    TUC-59 Moving computeConcentrations() method from CoreComponent to ConcentrationCalculator. It will avoid to have low level functions calling CoreComponent.

--- src/tucucore/concentrationcalculator.h
+++ src/tucucore/concentrationcalculator.h
@@ -7,13 +7,33 @@
 
 #include "tucucore/definitions.h"
 #include "tucucore/parameter.h"
+#include "tucucore/dosage.h"
+#include "tucucore/residualerrormodel.h"
+
+// For ConcentrationPredictionPtr. This class should be declared somewhere else
+#include "tucucore/iprocessingservices.h"
 
 namespace Tucuxi {
 namespace Core {
 
+
+
 class ConcentrationCalculator
 {
 public:
+
+
+    enum class ComputationResult { Success, Failure, Aborted };
+
+    static ComputationResult computeConcentrations(
+        ConcentrationPredictionPtr &_prediction,
+        int _nbPoints,
+        const IntakeSeries &_intakes,
+        const ParameterSetSeries& _parameters,
+        const Etas& _etas = Etas(0),
+        const ResidualErrorModel &_residualErrorModel = EMPTY_RESIDUAL_ERROR_MODEL,
+        const Deviation& _eps = 0,
+        const bool _isFixedDensity = 0);
 
 };
 
