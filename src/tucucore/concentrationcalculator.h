1cbff24cab49837637bf16979c0658d5a5bc681a
Yann Thoma <yann.thoma@heig-vd.ch>
2017-08-18 17:42:27 +0200

    TUC-97 Preparing the interface of ConcentrationCalculator. Also the method for points computation is already in a draft status.

--- src/tucucore/concentrationcalculator.h
+++ src/tucucore/concentrationcalculator.h
@@ -9,6 +9,7 @@
 #include "tucucore/parameter.h"
 #include "tucucore/dosage.h"
 #include "tucucore/residualerrormodel.h"
+#include "tucucore/sample.h"
 
 // For ConcentrationPredictionPtr. This class should be declared somewhere else
 #include "tucucore/iprocessingservices.h"
@@ -25,6 +26,18 @@ public:
 
     enum class ComputationResult { Success, Failure, Aborted };
 
+    ///
+    /// \brief computeConcentrations
+    /// \param _prediction The calculated concentrations
+    /// \param _nbPoints The number of points asked
+    /// \param _intakes The intakes series
+    /// \param _parameters The parameters series
+    /// \param _etas The etas to apply to the parameters
+    /// \param _residualErrorModel The residual error model to use with the epsilons
+    /// \param _eps The epsilon to modify the final concentrations
+    /// \param _isFixedDensity Indicates if the density of points could be changed within the method
+    /// \return The status of computation
+    ///
     static ComputationResult computeConcentrations(
         ConcentrationPredictionPtr &_prediction,
         int _nbPoints,
@@ -34,6 +47,25 @@ public:
         const ResidualErrorModel &_residualErrorModel = EMPTY_RESIDUAL_ERROR_MODEL,
         const Deviation& _eps = 0,
         const bool _isFixedDensity = 0);
+
+    ///
+    /// \brief pointsAtTimes Calculates concentrations at specific times
+    /// It just calculates the final residuals for the next cycle,
+    /// and the values at the time points requested. If the eta vector is not empty, then
+    /// the etas are applied to the parameters.
+    /// @param _concentrations concentrations 2d vector allocated within
+    /// @param _intakes intakes for entire curve
+    /// @param _parameters parameters for entire curve
+    /// @param _samples samples (measures) for the entire curve
+    /// @param _eta vector of etas
+    /// \return The status of computation
+    ///
+    static ComputationResult pointsAtTimes(
+            Concentrations& _concentrations,
+            const IntakeSeries& _intakes,
+            const ParameterSetSeries& _parameters,
+            const SampleSeries& _samples,
+            const Etas& _eta = Etas(0));
 
 };
 
--- src/tucucore/montecarlopercentilecalculator.cpp
+++ src/tucucore/montecarlopercentilecalculator.cpp
@@ -4,6 +4,7 @@
 
 #include "montecarlopercentilecalculator.h"
 
+#include "tucucore/definitions.h"
 
 namespace Tucuxi {
 namespace Core {
@@ -12,6 +13,60 @@ namespace Core {
 MonteCarloPercentileCalculator::MonteCarloPercentileCalculator()
 {
 
+}
+
+
+MonteCarloPercentileCalculator::ProcessingResult MonteCarloPercentileCalculator::computePredictionsAndSortPercentiles(
+        PercentilesPrediction _percentiles,
+        const int _nbPoints,
+        const IntakeSeries &_intakes,
+        const ParameterSetSeries &_parameters,
+        const ResidualErrorModel &_residualErrorModel,
+        const PercentileRanks &_percentileRanks,
+        int _nbPatients,
+        const std::vector<Etas> _etas,
+        const std::vector<Deviation> _epsilons,
+        int _curvelength,
+        ProcessingAborter *_aborter)
+{
+
+    return ProcessingResult::Failure;
+}
+
+MonteCarloPercentileCalculator::ProcessingResult AprioriMonteCarloPercentileCalculator::calculate(
+        PercentilesPrediction _percentiles,
+        const int _nbPoints,
+        const IntakeSeries &_intakes,
+        const ParameterSetSeries &_parameters,
+        const Omega& _omega,
+        const ResidualErrorModel &_residualErrorModel,
+        const Etas& _initialEta,
+        const PercentileRanks &_percentileRanks,
+        ProcessingAborter *_aborter)
+{
+
+
+    return ProcessingResult::Failure;
+}
+
+
+
+
+MonteCarloPercentileCalculator::ProcessingResult AposterioriMonteCarloPercentileCalculator::calculate(
+        PercentilesPrediction _percentiles,
+        const int _nbPoints,
+        const IntakeSeries &_intakes,
+        const ParameterSetSeries &_parameters,
+        const Omega& _omega,
+        const ResidualErrorModel &_residualErrorModel,
+        const Etas& _Eta,
+        const SampleSeries &_samples,
+        const PercentileRanks &_percentileRanks,
+        ProcessingAborter *_aborter)
+{
+
+
+    return ProcessingResult::Failure;
 }
 
 
