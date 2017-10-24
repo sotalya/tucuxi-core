06cdea22d7208bac49dfc661df65dc419803d751
Junghyun Kim <junghyun.kim@heig-vd.ch>
2017-10-24 11:10:27 +0200

    TUC-128 some bugs in cycle statistics and unit test have been fixed

--- src/tucucore/cyclestatisticscalculator.cpp
+++ src/tucucore/cyclestatisticscalculator.cpp
@@ -27,11 +27,13 @@ std::vector<TimeOffsets> &_times)
 
 	// if no concentrations, return... no data in concentrations
 	if (!(_concentrations[compartment]).size()) {
+	    std::cout << "No data in concentrations" << std::endl;
 	    return;
 	}
 	
 	Value prevGradient = 0.0, gradient = 0.0, auc = 0.0, peak = 0.0;
 	int peakPosition = 0;
+
 	for (unsigned int nbPoints = 0; nbPoints < (_concentrations[compartment].size() - 1); nbPoints++) {
 
 	    // compute current gradient
@@ -40,12 +42,10 @@ std::vector<TimeOffsets> &_times)
 	    // max: compare whether the gradient is changed from positive to negative
 	    if ((prevGradient > 0) && (gradient < 0)) {
 		m_stats[compartment][static_cast<int>(CycleStatisticType::Maximum)].addValue(Duration(std::chrono::seconds((int)(_times[compartment][nbPoints])*60*60)), _concentrations[compartment][nbPoints]);
-		std::cout << "max: " << _concentrations[compartment][nbPoints] << ", position: " << nbPoints << std::endl;
 	    }
 	    // min: compare whether the gradient is changed from negative to positive
-	    else if ((prevGradient > 0) && (gradient < 0)) {
+	    else if ((prevGradient < 0) && (gradient > 0)) {
 		m_stats[compartment][static_cast<int>(CycleStatisticType::Minimum)].addValue(Duration(std::chrono::seconds((int)(_times[compartment][nbPoints])*60*60)), _concentrations[compartment][nbPoints]);
-		std::cout << "min: " << _concentrations[compartment][nbPoints] << ", position: " << nbPoints << std::endl;
 	    }
 
 	    // store current gradient to previous gradient for next comparison
@@ -64,13 +64,12 @@ std::vector<TimeOffsets> &_times)
 		peak = _concentrations[compartment][nbPoints+1];
 		peakPosition = nbPoints + 1;
 	    }
-	    std::cout << "peak: " << peak << ", position: " << peakPosition << std::endl;
 	}
-	
+
 	// add AUC value with time 0
 	m_stats[compartment][static_cast<int>(CycleStatisticType::AUC)].addValue(Duration(), auc);
 	// add mean value with time 0
-	m_stats[compartment][static_cast<int>(CycleStatisticType::Mean)].addValue(Duration(), (auc /(_times[compartment][_concentrations[compartment].size()] - _times[compartment][0])));
+	m_stats[compartment][static_cast<int>(CycleStatisticType::Mean)].addValue(Duration(), (auc/(_times[compartment][_concentrations[compartment].size() - 1] - _times[compartment][0])));
 	// add peak value with duration (change offset to duration)
 	m_stats[compartment][static_cast<int>(CycleStatisticType::Peak)].addValue(Duration(std::chrono::seconds((int)(_times[compartment][peakPosition])*60*60)), peak);
     }
@@ -84,8 +83,10 @@ CycleStatistics::CycleStatistics(const CycleData &_data)
     //
     unsigned int nbCompartments = _data.m_concentrations.size();
 
-    m_stats.reserve(nbCompartments);
+    // m_stats.reserve(nbCompartments);
     for (unsigned int compartment = 0; compartment < nbCompartments; compartment++) {
+
+	m_stats.push_back(std::vector<Tucuxi::Core::CycleStatistic> ());
 	for (unsigned int type= 0; type< static_cast<int>(CycleStatisticType::CYCLE_STATISTIC_TYPE_SIZE); type++) {
 	    m_stats[compartment].push_back(CycleStatistic(_data.m_start, (CycleStatisticType)type));
 	}
