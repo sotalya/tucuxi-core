a54414b012766792ec306477c9e9838960a66850
Yann Thoma <yann.thoma@heig-vd.ch>
2018-09-25 11:19:13 +1000

    TUC-138 Implement a first version of SampleExtractor. Very basic, it just selects the samples inside a date range.

--- src/tucucore/sampleextractor.cpp
+++ src/tucucore/sampleextractor.cpp
@@ -10,13 +10,18 @@ namespace Tucuxi {
 namespace Core {
 
 
+// TODO : This function should be tested.
+// TODO : Nothing about units here...
 int SampleExtractor::extract(const Samples &_samples, const DateTime &_start, const DateTime &_end, SampleSeries &_series)
 {
-    TMP_UNUSED_PARAMETER(_samples);
-    TMP_UNUSED_PARAMETER(_start);
-    TMP_UNUSED_PARAMETER(_end);
-    TMP_UNUSED_PARAMETER(_series);
-    return 0;
+    int nbRelevantSamples = 0;
+    for (const auto & sample : _samples) {
+        if ((sample->getDate() > _start) && (sample->getDate() < _end)) {
+            _series.push_back(SampleEvent(sample->getDate(), sample->getValue()));
+            nbRelevantSamples ++;
+        }
+    }
+    return nbRelevantSamples;
 }
 
 
