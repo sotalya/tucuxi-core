77f4ba9cadf1647323a6d981a5ce0666470ac06f
Yann Thoma <yann.thoma@heig-vd.ch>
2018-10-29 11:43:55 +1100

    TUC-132 Units are now in their own .h/.cpp file. A translation function draft is now available

--- src/tucucore/unit.cpp
+++ src/tucucore/unit.cpp
@@ -1,6 +1,35 @@
 #include "unit.h"
 
-Unit::Unit()
+#include <iostream>
+
+namespace Tucuxi {
+namespace Core {
+
+Value translateToUnit(Value _value, Unit _initialUnit, Unit _finalUnit)
 {
+    if (_initialUnit == _finalUnit) {
+        return _value;
+    }
+
+    static std::map<std::string, Value> factorMap = {
+        {"mg/l-ug/l", 1000.0},
+        {"ug/l-mg/l", 0.0001}
+    };
+
+    std::string key = _initialUnit.toString() + "-" + _finalUnit.toString();
+    if (factorMap.count(key) == 0) {
+        std::cout << "Error in unit conversion. No known conversion from " <<
+                     _initialUnit.toString() << " to "  << _finalUnit.toString() << std::endl;
+        return 0.0;
+    }
+
+    return _value * factorMap[key];
+
 
 }
+
+
+
+
+} // namespace Core
+} // namespace Tucuxi
--- src/tucucore/unit.h
+++ src/tucucore/unit.h
@@ -1,11 +1,33 @@
-#ifndef UNIT_H
-#define UNIT_H
+#ifndef TUCUXI_CORE_UNIT_H
+#define TUCUXI_CORE_UNIT_H
 
+#include <string>
+
+#include "definitions.h"
+
+namespace Tucuxi {
+namespace Core {
 
 class Unit
 {
 public:
-    Unit();
+    Unit() {}
+    Unit(std::string _unitString) { m_unitString = _unitString; }
+
+    inline bool operator==(const Unit& rhs) const { return this->m_unitString == rhs.m_unitString; }
+
+    std::string toString() const { return m_unitString;}
+
+protected:
+    std::string m_unitString;
+
 };
 
-#endif // UNIT_H
+Value translateToUnit(Value _value, Unit _initialUnit, Unit _finalUnit);
+
+
+} // namespace Core
+} // namespace Tucuxi
+
+
+#endif // TUCUXI_CORE_UNIT_H
