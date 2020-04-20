af37969b10ca65f9813e0074afb6f827ca9626d7
Yann Thoma <yann.thoma@heig-vd.ch>
2020-04-20 15:14:30 +0200

    TUC-163 Preparing QueryResponse data

--- src/tucuquery/computingqueryresponse.cpp
+++ src/tucuquery/computingqueryresponse.cpp
@@ -5,6 +5,16 @@ namespace Tucuxi {
 namespace Query {
 
 
+SingleResponseData::SingleResponseData(
+        std::unique_ptr<Tucuxi::Core::SingleComputingResponse> _computingResponse,
+        Tucuxi::Core::ComputingResult _status,
+        std::unique_ptr<ComputingResponseMetaData> _metaData) :
+    m_computingResponse(std::move(_computingResponse)),
+    m_status(_status),
+    m_metaData(std::move(_metaData))
+{
+}
+
 Tucuxi::Core::RequestResponseId ComputingQueryResponse::getQueryId() const
 {
     return m_queryId;
@@ -13,6 +23,17 @@ Tucuxi::Core::RequestResponseId ComputingQueryResponse::getQueryId() const
 const std::vector<SingleResponseData>& ComputingQueryResponse::getRequestResponses() const
 {
     return m_requestResponses;
+}
+
+
+void ComputingQueryResponse::addRequestResponse(
+        std::unique_ptr<Tucuxi::Core::SingleComputingResponse> _computingResponse,
+        Tucuxi::Core::ComputingResult _status,
+        std::unique_ptr<ComputingResponseMetaData> _metaData)
+{
+    m_requestResponses.push_back(SingleResponseData(std::move(_computingResponse),
+                                  _status,
+                                  std::move(_metaData)));
 }
 
 } // namespace Query
--- src/tucuquery/computingqueryresponse.h
+++ src/tucuquery/computingqueryresponse.h
@@ -9,20 +9,35 @@ namespace Query {
 
 ///
 /// \brief The QueryResponseMetaData class
-/// This class will embed information such as computation time
+/// This class will embed information such as computation time for a ComputingQuery
 ///
 class QueryResponseMetaData
 {
 
 };
 
+///
+/// \brief The ComputingResponseMetaData class
+/// This class will embed information such as the drug model ID
+/// used for computation, and the computation time, for a single ComputingRequest
+///
+class ComputingResponseMetaData
+{
+
+    std::string m_drugModelId;
+};
+
 class SingleResponseData {
 
 public:
 
-    Tucuxi::Core::ComputingResult m_status;
-    QueryResponseMetaData m_metaData;
+    SingleResponseData(std::unique_ptr<Tucuxi::Core::SingleComputingResponse> _computingResponse,
+                       Tucuxi::Core::ComputingResult _status,
+                       std::unique_ptr<ComputingResponseMetaData> _metaData);
+
     std::unique_ptr<Tucuxi::Core::SingleComputingResponse> m_computingResponse;
+    Tucuxi::Core::ComputingResult m_status;
+    std::unique_ptr<ComputingResponseMetaData> m_metaData;
 };
 
 ///
@@ -39,15 +54,17 @@ public:
     Tucuxi::Core::RequestResponseId getQueryId() const;
     const std::vector<SingleResponseData>& getRequestResponses() const;
 
-    void addRequestResponse(std::unique_ptr<Tucuxi::Core::SingleComputingResponse>,
+    void addRequestResponse(std::unique_ptr<Tucuxi::Core::SingleComputingResponse> _computingResponse,
                             Tucuxi::Core::ComputingResult _status,
-                            std::unique_ptr<QueryResponseMetaData> _metaData);
+                            std::unique_ptr<ComputingResponseMetaData> _metaData);
 
     Tucuxi::Core::RequestResponseId m_queryId;
 
     Tucuxi::Core::ComputingResult m_queryStatus;
 
     std::vector<SingleResponseData> m_requestResponses;
+
+    std::unique_ptr<QueryResponseMetaData> m_metaData;
 };
 
 
