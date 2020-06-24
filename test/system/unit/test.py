f10cf270dcb05e0d03e315c6c93ea951ff5a1dc8
johan <johan.matthey@heig-vd.ch>
2020-06-24 09:36:43 +0200

    TUC-188 Small modifications

--- test/system/unit/test.py
+++ test/system/unit/test.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 
-import sys
 from argparse import ArgumentParser
-from subprocess import CalledProcessError
 from colorama import init
-import xml.dom.minidom
 from tucuxi.processing.tucuxirun import *
-from os import listdir
-from os.path import isfile, join
 import filecmp
 
-# globals
 
-parser = ArgumentParser(description='Tucuxi global tests comparing Tucuxi with NONMEM.',
-                        prog='tucuvalidation.py')
+parser = ArgumentParser(description='Tucuxi system test concerning data unit.',
+                        prog='test.py')
 
 # For colorama, to reset the color on every new print
 init(autoreset=True)
 
 foldername = ''
+
+app_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def parse_the_args():
@@ -26,8 +22,9 @@ def parse_the_args():
     * This method parses the command line arguments. This project relies on windows having unix tools
       available in the console. We get this by installing git and allowing to use it via cmd.exe.
     """
+    global app_path
     cli = 'tucucli'
-    drgsfldr = os.path.abspath('drugfiles')
+    drgsfldr = os.path.join(app_path, "..", "drugfiles")
 
     parser.add_argument('-d', type=str, dest='drugspath',
                         help='Import folder paths for Tucuxi drugfiles (tdd)',
@@ -38,43 +35,14 @@ def parse_the_args():
     return parser.parse_args()
 
 
-def process_results(data):
-    outputfilename = get_output_folder_name() + '/' + data[0]['queryId'] + '_results_adjustments.csv'
-    outputfile = open(outputfilename, 'w')
-
-    sep = ','
-
-    s = ''
-    s += 'drugModelId' + sep + 'dose' + sep + 'interval' + sep + 'infusion' + sep + 'score' + sep + \
-         'target' + sep + 'PK parameters' + '\n'
-
-    for patient in data:
-        result = patient['results']['adjustment']
-        if len(result) > 0:
-            r = result[0]
-            s += patient['drugModelId'] + sep + str(r[0]) + sep + str(r[1]) + sep + str(r[2]) + sep + \
-                str(r[3]) + sep + str(r[4])
-            for param_value in r[5]:
-                s += sep + str(param_value)
-            s += '\n'
-        else:
-            s += patient['drugModelId'] + sep + '-' + sep + '-' + sep + '-' + sep + '-' + sep + '-' + '\n'
-
-    outputfile.write(s)
-    outputfile.close()
-
-    pltr = Plotter(get_output_folder_name(), 'study_tucuxi', '')
-
-    pltr.plot_models(data, data[0]['queryId'] + '_models_comparison', 'Models comparison')
-
-
-def process_queryfile(args, query_file: str, output_folder: str, id):
-
+def process_queryfile(args, query_file: str, output_folder: str, query_id):
+    test_folder_name = query_file.split(os.path.sep)[-3]
+    test_query_name = query_file.split(os.path.sep)[-1]
+    print("\nRunning file '{qid}' from folder '{testfolder}'".format(qid=test_query_name,
+                                                                     testfolder=test_folder_name))
     tucucli = args.tucucli
     drug_path = args.drugspath
-    output_file_path = output_folder + '/' + "query" + str(id + 1) + '.xml'
-
-    print('')
+    output_file_path = output_folder + '/' + "query" + str(query_id + 1) + '.xml'
 
     tucuclirun = TucuCliRun(tucucli, drug_path, output_file_path)
     tucuclirun.run_tucuxi_from_file(query_file)
@@ -82,6 +50,7 @@ def process_queryfile(args, query_file: str, output_folder: str, id):
 
 def compare_query_responses(queries):
     once = True
+    print('\nRunning responses comparison ...')
     for i, test in enumerate(queries):
         for r, d, f in os.walk(test[1]):
             for file in f:
@@ -89,28 +58,29 @@ def compare_query_responses(queries):
                     print(Fore.RED + "Test{} : queries responses not equal".format(i + 1))
                     once = False
         once = True
+    print("Comparisons finished")
 
 
 def get_query_responses(args, queries):
     for test in queries:
-        for id, queryfile in enumerate(test[0]):
-            process_queryfile(args, queryfile, test[1], id)
+        for qid, queryfile in enumerate(test[0]):
+            process_queryfile(args, queryfile, test[1], qid)
 
 
 def get_queries_path(query_responses):
-    app_path = os.path.dirname(os.path.realpath(__file__))
+    global app_path
 
     for r, d, f in os.walk(app_path):
-        for i, dir in enumerate(d):
-            for r1, d1, f1 in os.walk(os.path.join(r, dir)):
-                for i1, dir1 in enumerate(d1):
-                    if dir1 == "in_tqf":
+        for i, dir1 in enumerate(d):
+            for r1, d1, f1 in os.walk(os.path.join(r, dir1)):
+                for i1, dir2 in enumerate(d1):
+                    if dir2 == "in_tqf":
                         query_responses.append([[]])
-                        for r2, d2, f2 in os.walk(os.path.join(r1, dir1)):
-                            for f, file in enumerate(f2):
+                        for r2, d2, f2 in os.walk(os.path.join(r1, dir2)):
+                            for file in f2:
                                 query_responses[i][i1].append(os.path.join(r2, file))
                     else:
-                        query_responses[i].append(os.path.join(r1, dir1))
+                        query_responses[i].append(os.path.join(r1, dir2))
 
 
 def main():
@@ -123,8 +93,6 @@ def main():
     get_query_responses(args, queries)
 
     compare_query_responses(queries)
-
-
 
     print(Fore.CYAN + 'End of script')
 
