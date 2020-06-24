3b6fe58488dbde31135fa55bdaef7540abc4e682
johan <johan.matthey@heig-vd.ch>
2020-06-24 16:17:45 +0200

    TUC-188 Test Time + cosmetics

--- test/system/unit/test.py
+++ test/system/unit/test.py
@@ -15,9 +15,9 @@ parser = ArgumentParser(description='Tucuxi system test concerning data unit.',
 # For colorama, to reset the color on every new print
 init(autoreset=True)
 
-foldername = ''
-
 app_path = os.path.dirname(os.path.realpath(__file__))
+output_folder_name = "output"
+input_folder_name = "in_tqf"
 
 
 def parse_the_args():
@@ -38,20 +38,29 @@ def parse_the_args():
     return parser.parse_args()
 
 
-def process_queryfile(args, query_file: str, output_folder: str, query_id):
+def process_queryfile(args, query_file: str, output_folder: str):
+    """
+    Input : args, query_file, output_folder
+    Generate query response for each query
+    """
     test_folder_name = query_file.split(os.path.sep)[-3]
     test_query_name = query_file.split(os.path.sep)[-1]
     print("\nRunning file '{qid}' from folder '{testfolder}'".format(qid=test_query_name,
                                                                      testfolder=test_folder_name))
     tucucli = args.tucucli
     drug_path = args.drugspath
-    output_file_path = output_folder + '/' + "query" + str(query_id + 1) + '.xml'
+    output_file_path = output_folder + '/' + test_query_name + '.xml'
 
     tucuclirun = TucuCliRun(tucucli, drug_path, output_file_path)
     tucuclirun.run_tucuxi_from_file(query_file)
 
 
 def compare_query_responses(queries):
+    """
+    Input : queries
+    Compare the first query response to the other (for each test)
+    """
+
     once = True
     print('\nRunning responses comparison ...')
     for i, test in enumerate(queries):
@@ -67,19 +76,25 @@ def compare_query_responses(queries):
 
 def get_query_responses(args, queries):
     for test in queries:
-        for qid, queryfile in enumerate(test[0]):
-            process_queryfile(args, queryfile, test[1], qid)
+        for queryfile in test[0]:
+            process_queryfile(args, queryfile, test[1])
 
 
-def get_queries_path(query_responses):
+def get_queries_path(queries):
+    """
+    Input : empty queries list
+    Fill queries list with all tests. Each test contain a list of queries path and the output path
+    """
+
     global app_path
-    output_folder_name = "output"
-    input_folder_name = "in_tqf"
+    global input_folder_name
+    global output_folder_name
+    tests_path = os.path.join(app_path, "tests")
 
-    list_test_folder = [f.name for f in os.scandir(app_path) if f.is_dir()]
+    list_test_folder = [f.name for f in os.scandir(tests_path) if f.is_dir()]
     for ti, testFolder in enumerate(list_test_folder):
-        query_responses.append([[]])
-        path_to_test_folder = os.path.join(app_path, testFolder)
+        queries.append([[]])
+        path_to_test_folder = os.path.join(tests_path, testFolder)
         path_to_queries = os.path.join(path_to_test_folder, input_folder_name)
         if not os.path.exists(path_to_queries):
             print(Fore.RED + "Missing '{miss}' folder in {test}".format(miss=input_folder_name, test=testFolder))
@@ -87,11 +102,10 @@ def get_queries_path(query_responses):
         path_to_output = os.path.join(path_to_test_folder, output_folder_name)
         if not os.path.exists(path_to_output):
             os.mkdir(path_to_output)
-
         list_files = [f for f in listdir(path_to_queries) if isfile(join(path_to_queries, f))]
-        query_responses[ti].append(os.path.join(app_path, testFolder, output_folder_name))
+        queries[ti].append(os.path.join(tests_path, testFolder, output_folder_name))
         for fi, file in enumerate(list_files):
-            query_responses[ti][0].append(os.path.join(path_to_queries, file))
+            queries[ti][0].append(os.path.join(path_to_queries, file))
 
 
 def main():
