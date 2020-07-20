63204ecd020027bdeb2edec58a1210b3d29dc4c2
Yann Thoma <yann.thoma@heig-vd.ch>
2020-07-20 08:53:59 +0200

    TUC-129 Modified build.sh

--- make/build.sh
+++ make/build.sh
@@ -7,18 +7,26 @@ echo $TUCUXI_ROOT
 mkdir -p $TUCUXI_ROOT/bin
 
 RESULT=0
-BUILD_LIBS=1
-BUILD_DOC=1
+BUILD_LIBS=0
+BUILD_DOC=0
+BUILD_TEST=0
+BUILD_APP=0
 
 for i in "$@" ; do
    if [ $i == "clean" ] ; then
       MAKECMD=clean
    fi
-   if [ $i == "nolib" ] ; then
-      BUILD_LIBS=0
+   if [ $i == "lib" ] ; then
+      BUILD_LIBS=1
    fi
-   if [ $i == "nodoc" ] ; then
-      BUILD_DOC=0
+   if [ $i == "doc" ] ; then
+      BUILD_DOC=1
+   fi
+   if [ $i == "test" ] ; then
+      BUILD_TEST=1
+   fi
+   if [ $i == "app" ] ; then
+      BUILD_APP=1
    fi
 done
 
@@ -34,25 +42,29 @@ if [ $BUILD_LIBS -eq 1 ] ; then
    done
 fi
 
-for MODULE in tucucommon tucucore tucuquery tucucli tucuvalidator tuculicense tucudrugfilechecker
-do
-   cd $TUCUXI_ROOT/src/$MODULE
-   mkdir -p objs
-   make TARGET=LINUX $MAKECMD 2>&1 | tee objs/build.log
-   if [ ${PIPESTATUS[0]} -eq 2 ] ; then
-      RESULT=1
-   fi
-done
+if [ $BUILD_APP -eq 1 ] ; then
+   for MODULE in tucucommon tucucore tucuquery tucucli tucuvalidator tuculicense tucudrugfilechecker
+   do
+      cd $TUCUXI_ROOT/src/$MODULE
+      mkdir -p objs
+      make TARGET=LINUX $MAKECMD 2>&1 | tee objs/build.log
+      if [ ${PIPESTATUS[0]} -eq 2 ] ; then
+         RESULT=1
+      fi
+   done
+fi
 
-for MODULE in tucucommon tucucore # tuculicense
-do
-   cd $TUCUXI_ROOT/test/$MODULE
-   mkdir -p objs
-   make TARGET=LINUX $MAKECMD 2>&1 | tee objs/build.log
-   if [ ${PIPESTATUS[0]} -eq 2 ] ; then
-      RESULT=1
-   fi
-done
+if [ $BUILD_TEST -eq 1 ] ; then
+   for MODULE in tucucommon tucucore # tuculicense
+   do
+      cd $TUCUXI_ROOT/test/$MODULE
+      mkdir -p objs
+      make TARGET=LINUX $MAKECMD 2>&1 | tee objs/build.log
+      if [ ${PIPESTATUS[0]} -eq 2 ] ; then
+         RESULT=1
+      fi
+   done
+fi
 
 if [ $BUILD_DOC -eq 1 ] ; then
    doxygen $TUCUXI_ROOT/src/doxyfile 2>&1 | tee objs/doxygen.log
