3f433b6be0c7d71b46d40d2422a58e7f917678db
Yann Thoma <yann.thoma@heig-vd.ch>
2020-08-27 15:09:23 +0200

    TUC-129 Intermediate commit

--- src/tucucore/pkmodels/rkmichaelismentenonecomp.cpp
+++ src/tucucore/pkmodels/rkmichaelismentenonecomp.cpp
@@ -1,6 +1,157 @@
 #include "rkmichaelismentenonecomp.h"
+#include "tucucore/intakeevent.h"
 
-RkMichaelisMentenOneComp::RkMichaelisMentenOneComp()
+namespace Tucuxi {
+namespace Core {
+
+
+RkMichaelisMentenOneComp::RkMichaelisMentenOneComp() : IntakeIntervalCalculatorRK4Base<2, RkMichaelisMentenOneComp> (new PertinentTimesCalculatorStandard())
 {
-
 }
+
+
+RkMichaelisMentenOneCompExtra::RkMichaelisMentenOneCompExtra() : RkMichaelisMentenOneComp()
+{
+}
+
+std::vector<std::string> RkMichaelisMentenOneCompExtra::getParametersId()
+{
+    return {"V", "Km", "Vmax", "F", "Ka"};
+}
+
+bool RkMichaelisMentenOneCompExtra::checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters)
+{
+    if (!checkValue(_parameters.size() >= 5, "The number of parameters should be equal to 5.")) {
+        return false;
+    }
+
+    m_D = _intakeEvent.getDose() * 1000;
+    m_V = _parameters.getValue(ParameterId::V);
+    m_Km = _parameters.getValue(ParameterId::Km);
+    m_Vmax = _parameters.getValue(ParameterId::Vmax);
+    m_F = _parameters.getValue(ParameterId::F);
+    m_Ka = _parameters.getValue(ParameterId::Ka);
+    m_nbPoints = _intakeEvent.getNbPoints();
+    m_Int = (_intakeEvent.getInterval()).toHours();
+
+    // check the inputs
+    bool bOK = checkValue(m_D >= 0, "The dose is negative.");
+    bOK &= checkValue(!std::isnan(m_D), "The dose is NaN.");
+    bOK &= checkValue(!std::isinf(m_D), "The dose is Inf.");
+    bOK &= checkValue(m_V > 0, "The volume is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_V), "The m_V is NaN.");
+    bOK &= checkValue(!std::isinf(m_V), "The _V is Inf.");
+    bOK &= checkValue(m_F > 0, "The volume is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_F), "The F is NaN.");
+    bOK &= checkValue(!std::isinf(m_F), "The F is Inf.");
+    bOK &= checkValue(m_Ke > 0, "The clearance is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_Ke), "The CL is NaN.");
+    bOK &= checkValue(!std::isinf(m_Ke), "The CL is Inf.");
+    bOK &= checkValue(m_Ka > 0, "The clearance is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_Ka), "The m_Ka is NaN.");
+    bOK &= checkValue(!std::isinf(m_Ka), "The m_Ka is Inf.");
+    bOK &= checkValue(m_nbPoints >= 0, "The number of points is zero or negative.");
+    bOK &= checkValue(m_Int > 0, "The interval time is negative.");
+
+    return bOK;
+}
+
+
+RkMichaelisMentenOneCompBolus::RkMichaelisMentenOneCompBolus() : RkMichaelisMentenOneComp()
+{
+}
+
+std::vector<std::string> RkMichaelisMentenOneCompBolus::getParametersId()
+{
+    return {"V", "Km", "Vmax", "F"};
+}
+
+bool RkMichaelisMentenOneCompBolus::checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters)
+{
+    if (!checkValue(_parameters.size() >= 4, "The number of parameters should be equal to 4.")) {
+        return false;
+    }
+
+    m_D = _intakeEvent.getDose() * 1000;
+    m_V = _parameters.getValue(ParameterId::V);
+    m_Km = _parameters.getValue(ParameterId::Km);
+    m_Vmax = _parameters.getValue(ParameterId::Vmax);
+    m_F = _parameters.getValue(ParameterId::F);
+    m_Ka = _parameters.getValue(ParameterId::Ka);
+    m_nbPoints = _intakeEvent.getNbPoints();
+    m_Int = (_intakeEvent.getInterval()).toHours();
+
+    // check the inputs
+    bool bOK = checkValue(m_D >= 0, "The dose is negative.");
+    bOK &= checkValue(!std::isnan(m_D), "The dose is NaN.");
+    bOK &= checkValue(!std::isinf(m_D), "The dose is Inf.");
+    bOK &= checkValue(m_V > 0, "The volume is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_V), "The m_V is NaN.");
+    bOK &= checkValue(!std::isinf(m_V), "The _V is Inf.");
+    bOK &= checkValue(m_F > 0, "The volume is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_F), "The F is NaN.");
+    bOK &= checkValue(!std::isinf(m_F), "The F is Inf.");
+    bOK &= checkValue(m_Ke > 0, "The clearance is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_Ke), "The CL is NaN.");
+    bOK &= checkValue(!std::isinf(m_Ke), "The CL is Inf.");
+    bOK &= checkValue(m_Ka > 0, "The clearance is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_Ka), "The m_Ka is NaN.");
+    bOK &= checkValue(!std::isinf(m_Ka), "The m_Ka is Inf.");
+    bOK &= checkValue(m_nbPoints >= 0, "The number of points is zero or negative.");
+    bOK &= checkValue(m_Int > 0, "The interval time is negative.");
+
+    return bOK;
+}
+
+
+RkMichaelisMentenOneCompInfusion::RkMichaelisMentenOneCompInfusion() : RkMichaelisMentenOneComp()
+{
+}
+
+std::vector<std::string> RkMichaelisMentenOneCompInfusion::getParametersId()
+{
+    return {"V", "Km", "Vmax", "F"};
+}
+
+bool RkMichaelisMentenOneCompInfusion::checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters)
+{
+    if (!checkValue(_parameters.size() >= 4, "The number of parameters should be equal to 4.")) {
+        return false;
+    }
+
+    m_D = _intakeEvent.getDose() * 1000;
+    m_V = _parameters.getValue(ParameterId::V);
+    m_Km = _parameters.getValue(ParameterId::Km);
+    m_Vmax = _parameters.getValue(ParameterId::Vmax);
+    m_F = _parameters.getValue(ParameterId::F);
+    m_Ka = _parameters.getValue(ParameterId::Ka);
+    m_nbPoints = _intakeEvent.getNbPoints();
+    m_Int = (_intakeEvent.getInterval()).toHours();
+
+    // check the inputs
+    bool bOK = checkValue(m_D >= 0, "The dose is negative.");
+    bOK &= checkValue(!std::isnan(m_D), "The dose is NaN.");
+    bOK &= checkValue(!std::isinf(m_D), "The dose is Inf.");
+    bOK &= checkValue(m_V > 0, "The volume is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_V), "The m_V is NaN.");
+    bOK &= checkValue(!std::isinf(m_V), "The _V is Inf.");
+    bOK &= checkValue(m_F > 0, "The volume is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_F), "The F is NaN.");
+    bOK &= checkValue(!std::isinf(m_F), "The F is Inf.");
+    bOK &= checkValue(m_Ke > 0, "The clearance is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_Ke), "The CL is NaN.");
+    bOK &= checkValue(!std::isinf(m_Ke), "The CL is Inf.");
+    bOK &= checkValue(m_Ka > 0, "The clearance is not greater than zero.");
+    bOK &= checkValue(!std::isnan(m_Ka), "The m_Ka is NaN.");
+    bOK &= checkValue(!std::isinf(m_Ka), "The m_Ka is Inf.");
+    bOK &= checkValue(m_nbPoints >= 0, "The number of points is zero or negative.");
+    bOK &= checkValue(m_Int > 0, "The interval time is negative.");
+
+    return bOK;
+}
+
+
+
+
+} // namespace Core
+} // namespace Tucuxi
--- src/tucucore/pkmodels/rkmichaelismentenonecomp.h
+++ src/tucucore/pkmodels/rkmichaelismentenonecomp.h
@@ -1,11 +1,140 @@
-#ifndef RKMICHAELISMENTENONECOMP_H
-#define RKMICHAELISMENTENONECOMP_H
+#ifndef TUCUXI_CORE_RKMICHAELISMENTENONECOMP_H
+#define TUCUXI_CORE_RKMICHAELISMENTENONECOMP_H
+
+#include "tucucore/intakeintervalcalculatorrk4.h"
+
+namespace Tucuxi {
+namespace Core {
 
 
-class RkMichaelisMentenOneComp
+enum class RkMichaelisMentenOneCompCompartments : int { First = 0, Second };
+
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenOneComp : public IntakeIntervalCalculatorRK4Base<2, RkMichaelisMentenOneComp>
 {
 public:
+    /// \brief Constructor
     RkMichaelisMentenOneComp();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+
+public:
+    inline void derive(double _t, const std::vector<double> &_c, std::vector<double>& _dcdt)
+    {
+        FINAL_UNUSED_PARAMETER(_t);
+        _dcdt[0] = m_Ka * _c[1] - m_Vmax * _c[0] / (m_Km + _c[0]);
+        _dcdt[1] = -m_Ka * _c[1];
+    }
+
+protected:
+
+
+    Value m_D;	/// Quantity of drug
+    Value m_F;  /// Biodisponibility
+    Value m_Ka; /// Absorption rate constant
+    Value m_V;  /// Volume of the compartment
+    Value m_Ke; /// Elimination constant rate = Cl/V where Cl is the clearance and V is the volume of the compartment
+    Value m_Km;
+    Value m_Vmax;
+    Value m_Tinf;
+
+
+private:
+    typedef RkMichaelisMentenOneCompCompartments Compartments;
 };
 
-#endif // RKMICHAELISMENTENONECOMP_H
+
+
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenOneCompExtra : public RkMichaelisMentenOneComp
+{
+    INTAKEINTERVALCALCULATOR_UTILS(RkMichaelisMentenOneCompExtra)
+public:
+    /// \brief Constructor
+    RkMichaelisMentenOneCompExtra();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+protected:
+    bool checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters) override;
+
+    void initConcentrations (const Residuals& _inResiduals, std::vector<double> &_concentrations) override
+    {
+        _concentrations[0] = _inResiduals[0];
+        _concentrations[1] = _inResiduals[1] + m_D / m_V * m_F;
+    }
+
+protected:
+
+};
+
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenOneCompBolus : public RkMichaelisMentenOneComp
+{
+    INTAKEINTERVALCALCULATOR_UTILS(RkMichaelisMentenOneCompBolus)
+public:
+    /// \brief Constructor
+    RkMichaelisMentenOneCompBolus();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+protected:
+    bool checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters) override;
+
+    void initConcentrations (const Residuals& _inResiduals, std::vector<double> &_concentrations) override
+    {
+        _concentrations[0] = _inResiduals[0] + m_D / m_V * m_F;
+        _concentrations[1] = _inResiduals[1];
+    }
+
+protected:
+
+};
+
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenOneCompInfusion : public RkMichaelisMentenOneComp
+{
+    INTAKEINTERVALCALCULATOR_UTILS(RkMichaelisMentenOneCompInfusion)
+public:
+    /// \brief Constructor
+    RkMichaelisMentenOneCompInfusion();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+protected:
+    bool checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters) override;
+
+    void initConcentrations (const Residuals& _inResiduals, std::vector<double> &_concentrations) override
+    {
+        _concentrations[0] = _inResiduals[0];
+        _concentrations[1] = _inResiduals[1];
+    }
+
+protected:
+
+};
+
+
+
+} // namespace Core
+} // namespace Tucuxi
+
+
+#endif // TUCUXI_CORE_RKMICHAELISMENTENONECOMP_H
