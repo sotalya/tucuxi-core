959e2d078b90c2e2b343c1771bc4a78ac15a6082
Yann Thoma <yann.thoma@heig-vd.ch>
2020-09-24 08:15:45 +0200

    TUC-129 Forgot parameterids.h

--- src/tucucore/parameterids.h
+++ src/tucucore/parameterids.h
@@ -14,8 +14,8 @@ namespace Core {
 class ParameterId 
 {
 public:
-    static const int size = 23;
-    enum Enum { V, V1, V2, CL, Ka, Ke, K12, K21, K13, K31, F, Q, Q1, Q2, Ktr, Tlag, TestA, TestM, TestR, TestS, TestT, a, b, Unknown };
+    static const int size = 25;
+    enum Enum { V, V1, V2, CL, Ka, Ke, K12, K21, K13, K31, F, Q, Q1, Q2, Ktr, Tlag, Km, Vmax, TestA, TestM, TestR, TestS, TestT, a, b, Unknown };
 
     static Enum fromString(const std::string &_id) {
         static const std::map<std::string, Enum> sm_paramIds = {  // NOLINT(readability-identifier-naming)
@@ -35,6 +35,8 @@ public:
             { "Q2",  ParameterId::Q2 },
             { "Ktr",  ParameterId::Ktr },
             { "Tlag", ParameterId::Tlag },
+            { "Km", ParameterId::Km },
+            { "Vmax", ParameterId::Vmax },
             { "TestA",  ParameterId::TestA },
             { "TestM",  ParameterId::TestM },
             { "TestR",  ParameterId::TestR },
