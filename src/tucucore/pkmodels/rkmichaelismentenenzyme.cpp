f63e1c70615879c8251e2cd9ecd37c8091a5e6b8
Yann Thoma <yann.thoma@heig-vd.ch>
2020-11-17 14:56:05 +0100

    TUC-222 First version of Michaelis Menten with enzyme

---
+++ src/tucucore/pkmodels/rkmichaelismentenenzyme.cpp
@@ -0,0 +1,205 @@
+#include "rkmichaelismentenenzyme.h"
+#include "tucucore/intakeevent.h"
+
+namespace Tucuxi {
+namespace Core {
+
+
+RkMichaelisMentenEnzyme::RkMichaelisMentenEnzyme() : IntakeIntervalCalculatorRK4Base<3, RkMichaelisMentenEnzyme> (new PertinentTimesCalculatorStandard())
+{
+}
+
+
+RkMichaelisMentenEnzymeExtra::RkMichaelisMentenEnzymeExtra() : RkMichaelisMentenEnzyme()
+{
+}
+
+std::vector<std::string> RkMichaelisMentenEnzymeExtra::getParametersId()
+{
+    return {"V", "Km", "Vmax", "F", "Ka", "Kenz", "Emax", "ECmid", "EDmid", "DoseMid", "Fmax", "NN", "MTT"};
+}
+
+Value toUgL(Value _v) { return _v * 1.0;}
+
+bool RkMichaelisMentenEnzymeExtra::checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters)
+{
+    if (!checkCondition(_parameters.size() >= 11, "The number of parameters should be equal to 10.")) {
+        return false;
+    }
+
+    m_D = toUgL(_intakeEvent.getDose());// * 1000.0;
+    m_V = _parameters.getValue(ParameterId::V);
+    m_Km = toUgL(_parameters.getValue(ParameterId::Km));// * 1000.0;
+    m_Vmax = toUgL(_parameters.getValue(ParameterId::Vmax));// * 1000.0;
+    m_F = _parameters.getValue(ParameterId::F);
+    m_Ka = _parameters.getValue(ParameterId::Ka);
+
+    m_Kenz = _parameters.getValue(ParameterId::Kenz);
+    m_Emax = _parameters.getValue(ParameterId::Emax);
+    m_ECmid = toUgL(_parameters.getValue(ParameterId::ECmid));// * 1000.0;
+    m_EDmid = toUgL(_parameters.getValue(ParameterId::EDmid));// * 1000.0;
+    m_DoseMid = toUgL(_parameters.getValue(ParameterId::DoseMid));// * 1000.0;
+    m_Fmax = _parameters.getValue(ParameterId::Fmax);
+    m_NN = _parameters.getValue(ParameterId::NN);
+    m_MTT = _parameters.getValue(ParameterId::MTT);
+
+    m_nbPoints = _intakeEvent.getNbPoints();
+    m_Int = (_intakeEvent.getInterval()).toHours();
+
+
+    m_ktr = (m_NN + 1.0) / m_MTT;
+    // Logarithm of the approximation to the gamma function
+    auto l = 0.9189385 + (m_NN + 0.5)*std::log(m_NN) - m_NN + std::log(1.0 + 1.0/(12.0 * m_NN));
+
+    Value tvbio;
+
+    // The formula for tvbio has a discontinuity
+    // We get rid of this by finding the critical point:
+    auto criticalPoint = m_DoseMid - m_EDmid;
+
+    // Then arbitrarily define a lower bound where the curve should be OK
+    auto lowBound = criticalPoint - toUgL(23.0);
+    // And a higher bound as well
+    auto highBound = criticalPoint + toUgL(37.0);
+    if ((m_D < criticalPoint) && (m_D > lowBound)) {
+        // If the dose is in [lowerBoud, criticalPoint], use the value at the bound
+        tvbio = m_F *(1.0 + m_Fmax * (lowBound - m_DoseMid) / (m_EDmid + (lowBound - m_DoseMid)));
+    }
+    else if ((m_D > criticalPoint) && (m_D < highBound)) {
+        // If the dose in is [criticalPoint, higherBound], use the value at the bound
+        tvbio = m_F *(1.0 + m_Fmax * (highBound - m_DoseMid) / (m_EDmid + (highBound - m_DoseMid)));
+    }
+    else {
+        // Else use the initial formula
+        tvbio = m_F *(1.0 + m_Fmax * (m_D - m_DoseMid) / (m_EDmid + (m_D - m_DoseMid)));
+    }
+
+    // Just a final check about the value
+    if (tvbio <= 0.0) {
+        tvbio = 0.5;
+    }
+
+    // TODO : Check how to deal with variability of bio here... OK, it should be on F
+    auto bio = tvbio;
+    auto lbpd = std::log(bio * m_D);
+    auto lktr = std::log(m_ktr);
+    m_cumul = lbpd + lktr - l;
+
+
+
+    // check the inputs
+    bool bOK = true;
+    bOK &= checkPositiveValue(m_D, "The dose");
+    bOK &= checkStrictlyPositiveValue(m_V, "The volume");
+    bOK &= checkStrictlyPositiveValue(m_F, "The bioavailability");
+    bOK &= checkStrictlyPositiveValue(m_Km, "The Michaelis Menten constant");
+    bOK &= checkStrictlyPositiveValue(m_Vmax, "VMax");
+    bOK &= checkStrictlyPositiveValue(m_Ka, "The absorption rate");
+    bOK &= checkStrictlyPositiveValue(m_Kenz, "Kenz");
+    bOK &= checkStrictlyPositiveValue(m_Emax, "Emax");
+    bOK &= checkStrictlyPositiveValue(m_ECmid, "ECmid");
+    bOK &= checkStrictlyPositiveValue(m_EDmid, "EDmid");
+    bOK &= checkStrictlyPositiveValue(m_DoseMid, "DoseMid");
+    bOK &= checkStrictlyPositiveValue(m_NN, "NN");
+    bOK &= checkStrictlyPositiveValue(m_MTT, "MTT");
+    bOK &= checkCondition(m_nbPoints >= 0, "The number of points is zero or negative.");
+    bOK &= checkCondition(m_Int > 0, "The interval time is negative.");
+
+    return bOK;
+}
+
+
+RkMichaelisMentenEnzymeBolus::RkMichaelisMentenEnzymeBolus() : RkMichaelisMentenEnzyme()
+{
+}
+
+std::vector<std::string> RkMichaelisMentenEnzymeBolus::getParametersId()
+{
+    return {"V", "Km", "Vmax", "F", "Kenz", "Emax", "ECmid"};
+}
+
+bool RkMichaelisMentenEnzymeBolus::checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters)
+{
+    if (!checkCondition(_parameters.size() >= 7, "The number of parameters should be equal to 7.")) {
+        return false;
+    }
+
+    m_D = _intakeEvent.getDose() * 1000;
+    m_V = _parameters.getValue(ParameterId::V);
+    m_Km = _parameters.getValue(ParameterId::Km);
+    m_Vmax = _parameters.getValue(ParameterId::Vmax);
+    m_F = _parameters.getValue(ParameterId::F);
+    m_Ka = 0.0; // _parameters.getValue(ParameterId::Ka);
+    m_DoseMid = 0.0;
+    m_EDmid = 0.0;
+    m_NN = 0.0;
+    m_MTT = 0.0;
+
+
+    m_Kenz = _parameters.getValue(ParameterId::Kenz);
+    m_Emax = _parameters.getValue(ParameterId::Emax);
+    m_ECmid = _parameters.getValue(ParameterId::ECmid);
+
+    m_nbPoints = _intakeEvent.getNbPoints();
+    m_Int = (_intakeEvent.getInterval()).toHours();
+
+    // check the inputs
+    bool bOK = true;
+    bOK &= checkPositiveValue(m_D, "The dose");
+    bOK &= checkStrictlyPositiveValue(m_V, "The volume");
+    bOK &= checkStrictlyPositiveValue(m_F, "The bioavailability");
+    bOK &= checkStrictlyPositiveValue(m_Km, "The Michaelis Menten constant");
+    bOK &= checkStrictlyPositiveValue(m_Vmax, "VMax");
+    bOK &= checkStrictlyPositiveValue(m_Kenz, "Kenz");
+    bOK &= checkStrictlyPositiveValue(m_Emax, "Emax");
+    bOK &= checkStrictlyPositiveValue(m_ECmid, "ECmid");
+    bOK &= checkCondition(m_nbPoints >= 0, "The number of points is zero or negative.");
+    bOK &= checkCondition(m_Int > 0, "The interval time is negative.");
+
+    return bOK;
+}
+
+/*
+RkMichaelisMentenEnzymeInfusion::RkMichaelisMentenEnzymeInfusion() : RkMichaelisMentenEnzyme()
+{
+}
+
+std::vector<std::string> RkMichaelisMentenEnzymeInfusion::getParametersId()
+{
+    return {"V", "Km", "Vmax", "F"};
+}
+
+bool RkMichaelisMentenEnzymeInfusion::checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters)
+{
+    if (!checkCondition(_parameters.size() >= 4, "The number of parameters should be equal to 4.")) {
+        return false;
+    }
+
+    m_D = _intakeEvent.getDose() * 1000;
+    m_V = _parameters.getValue(ParameterId::V);
+    m_Km = _parameters.getValue(ParameterId::Km);
+    m_Vmax = _parameters.getValue(ParameterId::Vmax);
+    m_F = _parameters.getValue(ParameterId::F);
+    m_Ka = _parameters.getValue(ParameterId::Ka);
+    m_nbPoints = _intakeEvent.getNbPoints();
+    m_Int = (_intakeEvent.getInterval()).toHours();
+
+    // check the inputs
+    bool bOK = true;
+    bOK &= checkPositiveValue(m_D, "The dose");
+    bOK &= checkStrictlyPositiveValue(m_V, "The volume");
+    bOK &= checkStrictlyPositiveValue(m_F, "The bioavailability");
+    bOK &= checkStrictlyPositiveValue(m_Km, "The Michaelis Menten constant");
+    bOK &= checkStrictlyPositiveValue(m_Vmax, "VMax");
+    bOK &= checkStrictlyPositiveValue(m_Ka, "The absorption rate");
+    bOK &= checkCondition(m_nbPoints >= 0, "The number of points is zero or negative.");
+    bOK &= checkCondition(m_Int > 0, "The interval time is negative.");
+
+    return bOK;
+}
+
+*/
+
+
+} // namespace Core
+} // namespace Tucuxi
---
+++ src/tucucore/pkmodels/rkmichaelismentenenzyme.h
@@ -0,0 +1,193 @@
+#ifndef TUCUXI_CORE_RKMICHAELISMENTENENZYME_H
+#define TUCUXI_CORE_RKMICHAELISMENTENENZYME_H
+
+#include "tucucore/intakeintervalcalculatorrk4.h"
+
+namespace Tucuxi {
+namespace Core {
+
+
+enum class RkMichaelisMentenEnzymeCompartments : int { First = 0, Second, Enzyme };
+
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenEnzyme : public IntakeIntervalCalculatorRK4Base<3, RkMichaelisMentenEnzyme>
+{
+public:
+    /// \brief Constructor
+    RkMichaelisMentenEnzyme();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+
+public:
+    inline void derive(double _t, const std::vector<double> &_c, std::vector<double>& _dcdt)
+    {
+        //auto c0 = _c[0] / 1000.0 * m_V;
+        //auto cp = c0 / m_V;
+        // We get concentration 0 in mg/l instead of ug/l
+        auto cp = _c[0] / 1000.0;
+        _dcdt[0] = m_Ka * _c[1] - m_Vmax * cp / (m_Km + cp) * _c[2]; // / m_V;
+        auto ktt = m_ktr * _t;
+        if (m_MTT == 0.0) {
+            _dcdt[1] = -m_Ka * _c[1];
+        }
+        else {
+            _dcdt[1] = -m_Ka * _c[1] + std::exp(m_cumul + m_NN * std::log(ktt) - ktt);
+        }
+        _dcdt[2] = m_Kenz * (1 + m_Emax * cp / (m_ECmid + cp)) - m_Kenz * _c[2];
+
+        // Here we modify the derivative on the central compartment, in order
+        // to consider it in ug/l     (1000.0 for ug, and /m_V for concentration)
+        _dcdt[0] *= 1000.0 / m_V;
+        assert(!std::isnan(_dcdt[0]));
+        assert(!std::isnan(_dcdt[1]));
+        assert(!std::isnan(_dcdt[2]));
+    }
+
+protected:
+
+
+    Value m_D;	/// Quantity of drug
+    Value m_F;  /// bioavailability
+    Value m_Ka; /// Absorption rate constant
+    Value m_V;  /// Volume of the compartment
+    Value m_Km;
+    Value m_Vmax;
+    Value m_Tinf;
+
+    Value m_Kenz;
+    Value m_Emax;
+    Value m_ECmid;
+    Value m_EDmid;
+    Value m_DoseMid;
+    Value m_Fmax;
+    Value m_NN;
+    Value m_MTT;
+
+    Value m_cumul;
+    Value m_ktr;
+
+
+private:
+    typedef RkMichaelisMentenEnzymeCompartments Compartments;
+};
+
+
+
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenEnzymeExtra : public RkMichaelisMentenEnzyme
+{
+    INTAKEINTERVALCALCULATOR_UTILS(RkMichaelisMentenEnzymeExtra)
+public:
+    /// \brief Constructor
+    RkMichaelisMentenEnzymeExtra();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+protected:
+    bool checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters) override;
+
+    void initConcentrations (const Residuals& _inResiduals, std::vector<double> &_concentrations) override
+    {
+        _concentrations[0] = _inResiduals[0];
+        _concentrations[1] = _inResiduals[1];
+        if (m_D > 0.0) {
+            if (_inResiduals[2] == 0.0) {
+                // First dose, we need an induction value
+                _concentrations[2] = 1.0;
+            }
+            else {
+                // Following dose
+                //_concentrations[2] = 0.0;
+                _concentrations[2] = _inResiduals[2];
+            }
+        }
+        else {
+            _concentrations[2] = _inResiduals[2];
+        }
+        // TODO : Check if this induction should be set to 1 everytime,
+        //        or only for the first dose
+        std::cout << _concentrations[2] << std::endl;
+    }
+
+protected:
+
+};
+
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenEnzymeBolus : public RkMichaelisMentenEnzyme
+{
+    INTAKEINTERVALCALCULATOR_UTILS(RkMichaelisMentenEnzymeBolus)
+public:
+    /// \brief Constructor
+    RkMichaelisMentenEnzymeBolus();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+protected:
+    bool checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters) override;
+
+    void initConcentrations (const Residuals& _inResiduals, std::vector<double> &_concentrations) override
+    {
+        _concentrations[0] = _inResiduals[0] + m_D * m_F / m_V;
+        _concentrations[1] = _inResiduals[1];
+        if (m_D > 0.0) {
+            _concentrations[2] = _inResiduals[2] + 1.0;
+        }
+        else {
+            _concentrations[2] = _inResiduals[2];
+        }
+    }
+
+protected:
+
+};
+
+/*
+/// \ingroup TucuCore
+/// \brief Intake interval calculator for the one compartment extravascular algorithm
+/// \sa IntakeIntervalCalculator
+class RkMichaelisMentenEnzymeInfusion : public RkMichaelisMentenEnzyme
+{
+    INTAKEINTERVALCALCULATOR_UTILS(RkMichaelisMentenEnzymeInfusion)
+public:
+    /// \brief Constructor
+    RkMichaelisMentenEnzymeInfusion();
+
+    /// \brief Returns the list of required PK parameters Ids
+    /// \return The list of required PK parameters Ids
+    static std::vector<std::string> getParametersId();
+
+protected:
+    bool checkInputs(const IntakeEvent& _intakeEvent, const ParameterSetEvent& _parameters) override;
+
+    void initConcentrations (const Residuals& _inResiduals, std::vector<double> &_concentrations) override
+    {
+        _concentrations[0] = _inResiduals[0];
+        _concentrations[1] = _inResiduals[1];
+
+    }
+
+protected:
+
+};
+*/
+
+
+} // namespace Core
+} // namespace Tucuxi
+
+
+#endif // TUCUXI_CORE_RKMICHAELISMENTENENZYME_H
