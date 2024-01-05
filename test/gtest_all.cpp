f15b5137b237d563bba960abfcea57473f37bb4c
Andrea Enrile <andrea.enrile@hes-so.ch>
2024-01-05 09:58:22 +0100

    Added gtest_all.cpp

---
+++ test/gtest_all.cpp
@@ -0,0 +1,13 @@
+//@@license@@
+
+#include <gtest/gtest.h>
+
+#include "tucucommon/utils.h"
+
+std::string appFolder;
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    appFolder = Tucuxi::Common::Utils::getAppFolder(argv);
+    return RUN_ALL_TESTS();
+}
