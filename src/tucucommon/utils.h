372459b0632ea6ba90aa973b66b0124c1566cc48
Annie Cathignol <annie.cathignol@heig-vd.ch>
2024-02-21 10:35:04 +0100

    removed issue on windows 'error C2039: '_snprintf': is not a member of 'std''.

--- src/tucucommon/utils.h
+++ src/tucucommon/utils.h
@@ -12,6 +12,13 @@
 
 #include "tucucore/definitions.h"
 
+#if defined(_MSC_VER) && _MSC_VER < 1900
+#define snprintf _snprintf
+#else
+#include <stdio.h> //sprintf
+#endif
+
+
 namespace Tucuxi {
 namespace Common {
 
@@ -100,9 +107,9 @@ std::string getAppFolder(char** _argv);
 template<typename... Args>
 std::string strFormat(const std::string& _format, Args... _args)
 {
-    size_t size = static_cast<size_t>(std::snprintf(nullptr, 0, _format.c_str(), _args...) + 1); // Extra space for '\0'
+    size_t size = static_cast<size_t>(snprintf(nullptr, 0, _format.c_str(), _args...) + 1); // Extra space for '\0'
     std::unique_ptr<char[]> buf(new char[size]);
-    std::snprintf(buf.get(), size, _format.c_str(), _args...);
+    snprintf(buf.get(), size, _format.c_str(), _args...);
     return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 }
 
