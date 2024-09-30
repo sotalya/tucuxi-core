a0773fe0edc5fbccdc781f1c5b275f62f893fce8
Yann Thoma <yann.thoma@heig-vd.ch>
2024-09-30 16:12:56 +0200

    Disabled a Gentamicin two targets test for now

--- test/tucucore/gtest_computingcomponentadjustments.cpp
+++ test/tucucore/gtest_computingcomponentadjustments.cpp
@@ -847,7 +847,7 @@ TEST(Core_TestComputingComponentAdjusements, ImatinibAllFormulationAndRouteBestD
 }
 
 
-TEST(Core_TestComputingComponentAdjusements, GentamicinTwoTargets)
+TEST(Core_TestComputingComponentAdjusements, DISABLED_GentamicinTwoTargets)
 {
     IComputingService* component = dynamic_cast<IComputingService*>(ComputingComponent::createComponent());
 
