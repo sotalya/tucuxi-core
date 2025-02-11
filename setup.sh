0966c7aa3edf1cfbdb0e59a627f26aab3724c789
Roberto Rigamonti <roberto.rigamonti@heig-vd.ch>
2025-02-11 11:59:07 +0100

    [fix] Set parameters in makefile to allow compilation with Mac

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTPATH/libs/botan
-python configure.py --without-documentation
-make -j$(nproc) -f Makefile
-
+python3 configure.py --without-documentation
+make -j 8 -f Makefile
